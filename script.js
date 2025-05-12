d4919b05cd60c99f5d56c6ef5617cce9490705cc
ankurmanglik <80889686+ankurmanglik@users.noreply.github.com>
2025-05-12 14:16:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const backToTopButton = document.getElementById("backToTop");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+      backToTopButton.classList.remove("hidden");
+    } else {
+      backToTopButton.classList.add("hidden");
+    }
+  });
+
+  backToTopButton.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+});
